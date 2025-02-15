0377126b714e02a0e764fe579e1a650b82c5fe6e
Kevin Barreto <barretokevin@hotmail.com>
2025-02-15 06:53:05 -0600

    fix adding to cart action

--- src/store/store.ts
+++ src/store/store.ts
@@ -16,9 +16,23 @@ export interface Store {
 const useStore = create<Store>((set) => ({
   cart: [],
   addToCart: (product) =>
-    set((state) => ({
-      cart: [...state.cart, { product, quantity: 1 }],
-    })),
+    set((state) => {
+      const existingItem = state.cart.find(
+        (item) => item.product.id === product.id
+      );
+
+      if (existingItem) {
+        return {
+          cart: state.cart.map((item) =>
+            item.product.id === product.id
+              ? { ...item, quantity: item.quantity + 1 }
+              : item
+          ),
+        };
+      }
+
+      return { cart: [...state.cart, { product, quantity: 1 }] };
+    }),
   removeFromCart: (productId) =>
     set((state) => ({
       cart: state.cart.filter((item) => +item.product.id !== +productId),
